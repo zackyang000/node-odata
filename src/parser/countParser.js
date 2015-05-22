0e2472a511c6ccaf90258168c3367b81ed4c11e9
Zack Yang <zackyang@outlook.com>
2015-05-22 11:20:33 +0800

    convert to es6.

---
+++ src/parser/countParser.js
@@ -0,0 +1,21 @@
+// # ?$skip=10
+// # ->
+// # query.skip(10)
+module.exports = function(resData, mongooseModel, $count, $filter) {
+  if(!$count)
+    return;
+
+  if($count === 'true') {
+    var query = mongooseModel.find();
+    require('./filterParser')(query, $filter);
+    query.count(function(err, count) {
+      resData['@odata.count'] = count;
+    });
+  }
+  else if($count == 'false') {
+    return;
+  }
+  else {
+    return new Error('Unknown $count option, only "true" and "false" are supported.');
+  }
+}
