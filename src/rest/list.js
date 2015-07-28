2aaef47f974204cbc614148ccb268291193daba6
Zack Yang <zackyang@outlook.com>
2015-07-28 16:18:29 +0800

    refacting: use promise.;

--- src/rest/list.js
+++ src/rest/list.js
@@ -9,14 +9,6 @@ import selectParser from '../parser/selectParser';
 
 export default (req, MongooseModel, options) => {
   return new Promise((resolve, reject) => {
-    let resData = {};
-
-    let query = MongooseModel.find();
-
-    let errHandle = (err) => {
-      return reject({status: 500}, {text: err});
-    };
-
     let params = {
       count: req.query.$count,
       filter: req.query.$filter,
@@ -28,44 +20,63 @@ export default (req, MongooseModel, options) => {
       // TODO search: req.query.$search,
     };
 
-    // TODO: use Promise
-    let err = countParser(resData, MongooseModel, params.count, req.query.$filter);
-    if(err) {
-      return errHandle(err);
-    }
-
-    err = filterParser(query, params.filter);
-    if(err) {
-      return errHandle(err);
-    }
-
-    err = orderbyParser(query, params.orderby || options.orderby);
-    if(err) {
-      return errHandle(err);
-    }
-
-    err = skipParser(query, params.skip, options.maxSkip);
-    if(err) {
-      return errHandle(err);
-    }
-
-    err = topParser(query, params.top, options.maxTop);
-    if(err) {
-      return errHandle(err);
-    }
-
-    err = selectParser(query, params.select);
-    if(err) {
-      return errHandle(err);
-    }
-
-    query.exec((err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resData.value = data;
-      return resolve({entity: resData});
+    let data = {};
+    Promise.all([
+      countQuery(MongooseModel, params),
+      dataQuery(MongooseModel, params, options),
+    ]).then(function(results) {
+      results.map(function(result) {
+        if (typeof result === 'number') {
+          data['@odata.count'] = result;
+        } else if (result instanceof Array) {
+          data.value = result;
+        }
+      });
+      return resolve({entity: data});
+    }).catch(function(err){
+      reject({status: 500}, {text: err});
     });
   });
 };
 
+function countQuery (model, { count, filter }) {
+  return countParser(model, count, filter);
+}
+
+function dataQuery (model, { filter, orderby, skip, top, select }, options) {
+  return new Promise((resolve, reject) => {
+    let query = model.find();
+
+    let err = filterParser(query, filter);
+    if(err) {
+      return reject(err);
+    }
+
+    err = orderbyParser(query, orderby || options.orderby);
+    if(err) {
+      return reject(err);
+    }
+
+    err = skipParser(query, skip, options.maxSkip);
+    if(err) {
+      return reject(err);
+    }
+
+    err = topParser(query, top, options.maxTop);
+    if(err) {
+      return reject(err);
+    }
+
+    err = selectParser(query, select);
+    if(err) {
+      return reject(err);
+    }
+
+    query.exec((err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
