518325d99b3b873bd0d917465a7c4d66fae4c9a5
Zack Yang <zackyang@outlook.com>
2016-01-26 20:44:03 +0800

    make eslint happy.

--- src/rest/put.js
+++ src/rest/put.js
@@ -1,39 +1,35 @@
-import { extend } from 'lodash';
-
-const uuidReg = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
-export default (req, MongooseModel) => {
-  return new Promise((resolve, reject) => {
-    MongooseModel.findOne({_id: req.params.id}, (err, entity) => {
-      if (err) {
-        return reject(err);
-      }
-
-      if (!entity) {
-        // create entity
-        if (!uuidReg.test(req.params.id)) {
-          return reject({ status: 400 }, { text: 'Id is not valid.' });
-        }
-        entity = new MongooseModel(req.body);
-        entity._id = req.params.id;
-        entity.save((err) => {
-          if (err) {
-            return reject(err);
-          }
-          return resolve({ status: 201, entity: entity });
-        });
-      } else {
-        // edit entity
-        let originEntity = JSON.parse(JSON.stringify(entity));
-        entity = extend(entity, req.body);
-        entity.save((err) => {
-          if (err) {
-            return reject(err);
-          }
-
-          return resolve({ entity, originEntity });
-        });
-      }
-    });
+function _updateEntity(resolve, reject, MongooseModel, req, entity) {
+  // TODO: 这样会导致 put 效果与 patch 相同.
+  MongooseModel.update({ id: entity.id }, req.body, (err) => {
+    if (err) {
+      return reject(err);
+    }
+    return resolve({ entity: req.body, originEntity: entity });
   });
-};
+}
+
+function _createEntity(resolve, reject, MongooseModel, req, entity) {
+  const uuidReg = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  if (!uuidReg.test(req.params.id)) {
+    return reject({ status: 400 }, { text: 'Id is not valid.' });
+  }
+  const newEntity = new MongooseModel(req.body);
+  newEntity._id = req.params.id;
+  newEntity.save((err) => {
+    if (err) {
+      return reject(err);
+    }
+    return resolve({ status: 201, entity: newEntity, originEntity: entity });
+  });
+}
+
+export default (req, MongooseModel) => new Promise((resolve, reject) => {
+  MongooseModel.findOne({ _id: req.params.id }, (err, entity) => {
+    if (err) {
+      return reject(err);
+    }
+    return entity
+      ? _updateEntity(resolve, reject, MongooseModel, req, entity)
+      : _createEntity(resolve, reject, MongooseModel, req, entity);
+  });
+});
