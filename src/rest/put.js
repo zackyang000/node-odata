0b2b09d422f46577bfdd40a8c13fbf5b0f3f6581
Zack Yang <zackyang@outlook.com>
2016-01-27 21:01:03 +0800

    remove commit and edit msg.

--- src/rest/put.js
+++ src/rest/put.js
@@ -1,5 +1,4 @@
 function _updateEntity(resolve, reject, MongooseModel, req, entity) {
-  // TODO: 这样会导致 put 效果与 patch 相同.
   MongooseModel.update({ id: entity.id }, req.body, (err) => {
     if (err) {
       return reject(err);
@@ -11,7 +10,7 @@ function _updateEntity(resolve, reject, MongooseModel, req, entity) {
 function _createEntity(resolve, reject, MongooseModel, req, entity) {
   const uuidReg = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
   if (!uuidReg.test(req.params.id)) {
-    return reject({ status: 400 }, { text: 'Id is not valid.' });
+    return reject({ status: 400 }, { text: 'Id is invalid.' });
   }
   const newEntity = new MongooseModel(req.body);
   newEntity._id = req.params.id;
