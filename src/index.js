f98f1f56af6b8b3e944e37bab49c5464d92a91f7
Zack Yang <zackyang@outlook.com>
2016-02-03 19:13:44 +0800

    refacting code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Server from './server';
-import Resource from './Resource';
-import Func from './Function';
+import Resource from './ODataResource';
+import Func from './ODataFunction';
 
 const server = function server(db, prefix) {
   return new Server(db, prefix);
