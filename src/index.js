a16401ce84da5e5b9f26a6282abb5ebe15c20348
Rodrigo Alfonso <rodri042@gmail.com>
2016-02-17 19:19:58 -0300

    Fix require's file name case for UNIX-like filesystems

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Server from './server';
-import Resource from './Resource';
-import Func from './Function';
+import Resource from './resource';
+import Func from './function';
 
 const server = function server(db, prefix) {
   return new Server(db, prefix);
