f9eca6af6e5b1ebd683e0a6422b57262c3b21a28
Zack Yang <zackyang@outlook.com>
2016-08-07 08:13:46 +0800

    test(model): use await/async to resolve ajax.

--- test/model.complex.js
+++ test/model.complex.js
@@ -1,50 +1,74 @@
+'use strict';
+
 // For issue: https://github.com/TossShinHwa/node-odata/issues/55
+require('should');
+const request = require('supertest');
 
-var should = require('should');
-var request = require('supertest');
-var odata = require('../.');
-var support = require('./support');
+require('./support');
+const odata = require('../.');
 
-var PORT = 0;
+let PORT = 0;
 
 describe('model.custom.id', () => {
   before((done) => {
-    var server = odata('mongodb://localhost/odata-test');
+    const server = odata('mongodb://localhost/odata-test');
     server.resource('complex-model', {
-        p1: [{ p2: String }],
-    })
-    var s = server.listen(PORT, () => {
-      PORT = s.address().port;
+      p1: [{ p2: String }],
+    });
+    const result = server.listen(PORT, () => {
+      PORT = result.address().port;
       done();
     });
   });
 
-  it('should work when PUT something', (done) => {
-    request(`http://localhost:${PORT}`)
-      .post('/complex-model')
-      .send({p1: [{ p2: 'origin' }]})
-      .expect(201)
-      .end((err, res) => {
-        if(err) {
-          return done(err);
-        }
-        var id = res.body.id;
-        request(`http://localhost:${PORT}`)
-          .put(`/complex-model(${id})`)
-          .send({p1: [{ p2: 'new' }]})
-          .expect(200)
-          .end((err, res) => {
-            if(err) {
-              return done(err);
-            }
-            request(`http://localhost:${PORT}`)
-              .get(`/complex-model(${id})`)
-              .expect(200)
-              .end((err, res) => {
-                res.body.p1[0].p2.should.be.equal('new');
-                done();
-              });
-          });
-      });
+  it('should work when PUT something', async function() {
+    const entity = await addResource();
+    await updateResouce(entity.body.id);
+    const res = await queryResource(entity.body.id);
+    res.body.p1[0].p2.should.be.equal('new');
   });
 });
+
+function addResource() {
+  return new Promise((resolve, reject) => {
+    return request(`http://localhost:${PORT}`)
+    .post('/complex-model')
+    .send({ p1: [{ p2: 'origin' }] })
+    .expect(201)
+    .end((err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(res);
+    });
+  });
+}
+
+function updateResouce(id) {
+  return new Promise((resolve, reject) => {
+    return request(`http://localhost:${PORT}`)
+    .put(`/complex-model(${id})`)
+    .send({ p1: [{ p2: 'new' }] })
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(res);
+    });
+  });
+}
+
+function queryResource(id) {
+  return new Promise((resolve, reject) => {
+    return request(`http://localhost:${PORT}`)
+    .get(`/complex-model(${id})`)
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(res);
+    });
+  });
+}
