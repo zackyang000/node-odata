106b07fe7ba8422ad5b3f9d227c87052a76c5179
Zack Yang <zackyang@outlook.com>
2016-09-06 15:30:20 +0800

    test(all): use await/async to resolve request.

---
+++ test/odata.actions.test.js
@@ -0,0 +1,42 @@
+import 'should';
+import request from 'supertest';
+import { odata, conn, host, port, bookSchema, initData } from './support/setup';
+
+let data;
+
+function requestToHalfPrice(id) {
+  return request(host).post(`/book(${id})/50off`);
+}
+
+function halfPrice(price) {
+  return +(price / 2).toFixed(2);
+}
+
+describe('odata.actions', function() {
+  let httpServer;
+
+  before(async function() {
+    data = await initData();
+    const server = odata(conn);
+    server
+    .resource('book', bookSchema)
+    .action('/50off', function(req, res, next) {
+      server.resources.book.findById(req.params.id, function(err, book) {
+        book.price = halfPrice(book.price);
+        book.save((err) => res.jsonp(book));
+      });
+    });
+    httpServer = server.listen(port);
+  });
+
+  after(() => {
+    httpServer.close();
+  });
+
+  it('should work', async function() {
+    const item = data[0];
+    const res = await requestToHalfPrice(item.id);
+    const price = halfPrice(item.price);
+    res.body.price.should.be.equal(price);
+  });
+});
