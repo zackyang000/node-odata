9d00d8a0b1fee93880ee14c7332b1a320fe0b364
Zack Yang <zackyang@outlook.com>
2016-09-08 21:34:46 +0800

    test(all): use await/async to resolve request.

---
+++ test/odata.query.filter.functions.test.js
@@ -0,0 +1,53 @@
+import 'should';
+import request from 'supertest';
+import { odata, conn, host, port, bookSchema, initData } from './support/setup';
+
+
+describe('odata.query.filter.functions', function() {
+  let data, httpServer;
+
+  before(async function() {
+    data = await initData();
+    const server = odata(conn);
+    server.resource('book', bookSchema)
+    httpServer = server.listen(port);
+  });
+
+  after(() => {
+    httpServer.close();
+  });
+
+  describe('[contains]', () => {
+    it('should filter items', async function() {
+      const res = await request(host).get(`/book?$filter=contains(title,'i')`); 
+      res.body.value.length.should.greaterThan(0);
+      res.body.value.should.matchEach((item) => item.title.indexOf('i') >= 1);
+    });
+    it('should filter items when it has extra spaces in query string', async function() {
+      const res = await request(host).get(`/book?$filter=contains(title,'Visual Studio')`); 
+      res.body.value.length.should.greaterThan(0);
+      res.body.value.should.matchEach((item) => item.title.indexOf('Visual Studio') >= 0);
+    });
+  });
+
+  describe('[indexof]', () => {
+    it('should filter items', async function() {
+      const res = await request(host).get(`/book?$filter=indexof(title,'i') ge 1`); 
+      res.body.value.length.should.greaterThan(0);
+      res.body.value.should.matchEach((item) => item.title.indexOf('i') >= 1);
+    });
+    it('should filter items when it has extra spaces in query string', async function() {
+      const res = await request(host).get(`/book?$filter=indexof(title,'Visual Studio') ge 0`); 
+      res.body.value.length.should.greaterThan(0);
+      res.body.value.should.matchEach((item) => item.title.indexOf('Visual Studio') >= 0);
+    });
+  });
+
+  describe('[year]', () => {
+    it('should filter items', async function() {
+      const res = await request(host).get(`/book?$filter=year(publish_date) eq 2000`); 
+        res.body.value.length.should.greaterThan(0);
+        res.body.value.should.matchEach((item) => new Date(item.publish_date).getFullYear() === 2000);
+    });
+  });
+});
