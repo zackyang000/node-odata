a91175cb1d1e1c49420aac315922795e6ee348e2
Zack Yang <zackyang@outlook.com>
2016-09-12 07:35:31 +0800

    test(all): use await/async to resolve request.

---
+++ test/odata.query.count.test.js
@@ -0,0 +1,33 @@
+import 'should';
+import request from 'supertest';
+import { odata, conn, host, port, bookSchema, initData } from './support/setup';
+
+describe('odata.query.filter.functions', function() {
+  let data, httpServer;
+
+  before(async function() {
+    data = await initData();
+    const server = odata(conn);
+    server.resource('book', bookSchema)
+    httpServer = server.listen(port);
+  });
+
+  after(() => {
+    httpServer.close();
+  });
+
+  it('should get count', async function() {
+    const res = await request(host).get('/book?$count=true');
+    res.body.should.be.have.property('@odata.count');
+    res.body.should.be.have.property('value');
+    res.body['@odata.count'].should.be.equal(res.body.value.length);
+  });
+  it('should not get count', async function() {
+    const res = await request(host).get('/book?$count=false');
+    res.body.should.be.not.have.property('@odata.count');
+  });
+  it('should 500 when $count isn\'t \'true\' or \'false\'', async function() {
+    const res = await request(host).get('/book?$count=1');
+    res.error.status.should.be.equal(500);
+  });
+});
