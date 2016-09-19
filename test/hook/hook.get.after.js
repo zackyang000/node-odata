f6c4cfe51f0a13b3990ef061df4aa8bd31cc41ad
Zack Yang <zackyang@outlook.com>
2016-09-20 06:14:59 +0800

    test(hooks): use await/async to resolve request and use sinon to resolve callback.

---
+++ test/hook/hook.get.after.js
@@ -0,0 +1,36 @@
+import 'should';
+import 'should-sinon';
+import request from 'supertest';
+import sinon from 'sinon';
+import { odata, conn, host, port, bookSchema, initData } from '../support/setup';
+
+describe('hook.get.after', function() {
+  let data, httpServer, server;
+
+  beforeEach(async function() {
+    data = await initData();
+    server = odata(conn);
+  });
+
+  afterEach(() => {
+    httpServer.close();
+  });
+
+  it('should work', async function() {
+    const callback = sinon.spy();
+    server.resource('book', bookSchema).get().after((entity) => {
+      entity.should.be.have.property('title');
+      callback();
+    });
+    httpServer = server.listen(port);
+    await request(host).get(`/book(${data[0].id})`);
+    callback.should.be.called();
+  });
+  it('should work with multiple hooks', async function() {
+    const callback = sinon.spy();
+    server.resource('book', bookSchema).get().after(callback).after(callback);
+    httpServer = server.listen(port);
+    await request(host).get(`/book(${data[0].id})`);
+    callback.should.be.calledTwice();
+  });
+});
