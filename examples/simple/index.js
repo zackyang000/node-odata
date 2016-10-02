e3af9d5c17353b9d7985ae55ec2aa7b4b2f89ebf
Zack Yang <zackyang@outlook.com>
2016-10-02 15:01:34 +0800

    chroe(example): #62, add a example for error handle.

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -25,6 +25,13 @@ server.get('/license', function(req, res, next){
   res.jsonp({license:'MIT'});
 });
 
+server.on('connected', function() {
+  console.log('MongoDB connected!');
+});
+server.on('disconnected', function() {
+  console.log('MongoDB disconnected!');
+});
+
 server.listen(3000, function(){
   console.log('OData services has started, you can visit by http://localhost:3000/book');
 });
