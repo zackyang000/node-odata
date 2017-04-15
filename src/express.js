030428878c7998f1afb5f8393191c24879cfd21d
Andriy Fetsyuk <AndreusAnd@gmail.com>
2017-04-15 14:36:37 +0300

    refactored setting cors options

--- src/express.js
+++ src/express.js
@@ -7,15 +7,12 @@ export default function (options) {
   const app = express();
   const opts = (options && options.expressRequestLimit) ?
                 { limit: options.expressRequestLimit } : {};
-  const corsOrigin = (options && options.corsOrigin) || '*';
   app.use(bodyParser.json(opts));
   opts.extended = true;
   app.use(bodyParser.urlencoded(opts));
   app.use(methodOverride());
   app.use(express.query());
-  app.use(cors({
-    origin: corsOrigin,
-  }));
+  app.use(cors(options && options.corsOptions));
   app.disable('x-powered-by');
   return app;
 }
