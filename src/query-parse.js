c24a5c765efa360dacdfe798a13d9e3db1df311d
Zack Yang <zackyang@outlook.com>
2017-05-13 17:16:49 +0800

    docs(parser): init jsDoc for parser.

--- src/query-parse.js
+++ src/query-parse.js
@@ -1,8 +1,55 @@
-export default function parse(query) {
-  // {
-  //   $top: 13,
-  //   $skip: 23,
-  // }
+/** @module parser */
+
+/**
+ * @typedef {Object} oDataQueryObject
+ * @property {Number=} top
+ *           1
+ * @property {Number=} skip
+ *           1
+ * @property {Array.<string>=} select
+ *           1
+ * @property {Object=} orderby
+ * @property {string} orderby.field
+ * @property {bool} orderby.asc
+ *           1
+ * @property {Number=} count
+ *           1
+ * @property {Array.<Object>=} filter
+ * @property {key} filter.key
+ * @property {key} filter.operator
+ * @property {key} filter.value
+ */
+
+/**
+ * Convert query string to OData format.
+ *
+ * @param {Object} query
+ *        query object which be processed by query parser.
+ * @param {string=} query.$top
+ *        1
+ * @param {string=} query.$skip
+ *        1
+ * @param {string=} query.$select
+ *        1
+ * @param {string=} query.$orderby
+ *        1
+ * @param {string=} query.$count
+ *        1
+ * @param {string=} query.$filter
+ *        1
+ * @param {Object=} options
+ *        default oData query setting.
+ * @param {string=} options.maxTop
+ *        1
+ * @param {string=} options.maxSkip
+ *        1
+ * @param {string=} options.defaultOrderBy
+ *        1
+ * @return {oDataQueryObject}
+ *         oData query object.
+*/
+export default function parse(query, options) {
+  const { $top, $skip, $select, $orderby, $count, $filter } = query;
   const result = {};
   result.top = parseTop(query.$top);
 }
