0750ab068e75350b4dc5f8e9696255e28def54b9
Zack Yang <zackyang@outlook.com>
2017-05-18 20:02:43 +0800

    feat(parse): add $orderby, $count parser.

--- src/query-parse.js
+++ src/query-parse.js
@@ -1,4 +1,5 @@
 import { min } from './utils';
+import createError from 'http-errors';
 
 /** @module parser */
 
@@ -10,7 +11,7 @@ import { min } from './utils';
  *           a non-negative integer n that excludes the first n items of the queried collection from the result
  * @property {Array.<string>=} select
  *           requests that the service return only the properties explicitly requested by the client
- * @property {Object=} orderby
+ * @property {Array.<Object>=} orderby
  *           specifies the order in which items are returned from the service
  * @property {string} orderby.field
  *           field name for order
@@ -51,6 +52,7 @@ export default function parse(query, options) {
 }
 
 const parser = [
+  // $top=10 -> { top: 10 }
   ({ $top }, { maxTop }) => {
     let top = parseToInteger($top);
 
@@ -65,6 +67,7 @@ const parser = [
     return { top };
   },
 
+  // $skip=10 -> { skip: 10 }
   ({ $skip }, { maxSkip }) => {
     let skip = parseToInteger($skip);
 
@@ -79,17 +82,62 @@ const parser = [
     return { skip };
   },
 
+  // $select=Rating,ReleaseDate -> { select: ['Rating', 'ReleaseDate'] }
   ({ $select }) => {
     if (!$select) {
       return;
     }
-    return $select.split(',').map(item => item.trim()).filter((item) => item);
+
+    const select = $select.split(',').map(item => item.trim()).filter((item) => item);
+
+    return { select };
   },
 
+  // $orderby=ReleaseDate asc, Rating desc -> { orderby: [{ field: 'ReleaseDate', asc: true }, field: 'Rating', asc: false }] }
   ({ $orderby }, { defaultOrderBy }) => {
+    const orderbyQuery = $orderby || defaultOrderBy;
+
+    if (orderbyQuery === undefined) {
+      return;
+    }
+
+    const orderby = orderbyQuery.split(',').map((item) => {
+      const data = item.trim().split(' ');
+
+      if (data.length > 2) {
+        throw createError.BadRequest(`odata: Syntax error at '${$orderby}', it\'s should be like \'ReleaseDate asc, Rating desc\'`);
+      }
+
+      // If it is not `desc`, then all other words are default assigned to `asc`.
+      return {
+        field: data[0].trim(),
+        asc: data[1] !== 'desc';
+      };
+    });
+
+    return { orderby };
   },
+
+  // $count=true -> { count: true }
   ({ $count }) => {
+    if ($count === undefined) return;
+
+    switch ($count) {
+      case 'true': {
+        return { count: true };
+        break;
+      }
+      case 'false':
+        return { count: false };
+        break;
+      default:
+        throw createError.BadRequest('Unknown $count option, only "true" and "false" are supported.');
+        break;
+    }
   },
+
+  // TODO: NOT SUPPORT `OR` KEYWORDS FOR THIS VERSION.
+  // $filter=Name eq 'John' and LastName lt 'Doe' -> [{ key: 'Name', operator: 'eq', value: 'John' }, { key: 'LastName', operator: 'let', value: 'Doe' }]
   ({ $filter }) => {
   },
 ]
