060c89c82dab434c011dd6b2ee903551baab3073
Zack Yang <zackyang@outlook.com>
2017-06-11 19:02:41 +0800

    test(basic): improve test code.

--- test/basic.js
+++ test/basic.js
@@ -1,31 +1,25 @@
 require('should');
 const request = require('supertest');
-const { conn, host, port, bookSchema, initData } = require('./support/setup');
-const odata = require('../index');
-const { Resource, queryable, action } = odata;
+const { conn, bookSchema, initData } = require('./support/setup');
+const odata = require('../');
 
-class Book extends Resource {
+class Book extends odata.Resource {
   constructor() {
-    super('book', bookInfo);
+    super('book', bookSchema);
   }
 }
 
 describe('rest.get', () => {
-  let data, httpServer;
+  let data, app;
 
   before(async function() {
     data = await initData();
-    const server = odata(conn);
-    server.registerResource([Book]);
-    httpServer = server.listen(port);
-  });
-
-  after(() => {
-    httpServer.close();
+    app = odata(conn);
+    app.use([Book]);
   });
 
   it('should return all of the resources', async function() {
-    const res = await request(host).get(`/book`);
+    const res = await request(app).get(`/book`);
     res.body.should.be.have.property('value');
     res.body.value.length.should.be.equal(data.length);
   });
